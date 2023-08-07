d54f53c946e7cbfa1dd6b52ef02c5c5c2b82d8f9
Luca Kato <lucakato@umich.edu>
2023-08-07 18:22:07 -0400

    Testing artist top song search

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # spotify-db
-Track listening history
+Track listening history and artist top tracks on Spotify
+This is only for client use and does not allow authenticate any user.
+.env file has been removed because it contains keys needed for API use
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 import base64
 import json
-from requests import post
+from requests import post, get
 load_dotenv()
 
 client_id = os.getenv("CLIENT_ID")
@@ -25,5 +25,37 @@ def get_token():
     token = json_result["access_token"]
     return token
 
+# For sending requests to API
+# construct header
+def get_auth_header(token):
+    return {"Authorization": "Bearer " + token}
+
+# returns ID for artist
+def search_for_artist(token, artist_name):
+    url = "https://api.spotify.com/v1/search?"
+    headers = get_auth_header(token)
+    query = f"q={artist_name}&type=artist&limit=1"
+
+    query_url = url + query
+    result = get(query_url, headers=headers)
+    json_res = json.loads(result.content)["artists"]["items"]
+    if len(json_res) == 0:
+        print("Could not find artist")
+        return None
+    return json_res[0]
+
+
+def getTopSongsArtist(token, id):
+    headers = get_auth_header(token)
+    query_url = f"https://api.spotify.com/v1/artists/{id}/top-tracks?country=JP"
+    res = get(query_url, headers=headers)
+    json_res = json.loads(res.content)["tracks"]
+    return json_res
+
 token = get_token()
-print(token)
+res = search_for_artist(token, "Carlo Redl")
+artist_id = res["id"]
+songs = getTopSongsArtist(token, artist_id)
+
+for idx, song in enumerate(songs):
+    print(f"{idx+1}. {song['name']}")
